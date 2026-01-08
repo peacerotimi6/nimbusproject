0cdfee11102c361236fdad59c93f73b8a98fe909
Samuel Igbinore <your-github-email@example.com>
2026-01-08 17:00:22 -0500

    pushing my terraform code

---
+++ Modules/keyvaults/variable.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  description = "Prefix name for the keyvault"
+  type = string
+  
+}
+variable "location" {
+  description = "Azure region"
+  type = string
+  
+}
+variable "resource_group_name" {
+  description = "Name of the Resource group"
+  type = string
+  
+}
